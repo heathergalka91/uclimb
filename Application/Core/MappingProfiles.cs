70d07d35066fe8066cbc9e7f23e655c0d4268aa3
Heather Gakla <Heather.Galka.91@gmail.com>
2022-07-07 10:42:19 -0600

    end of section 16

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Application.Activities;
 using AutoMapper;
 using Domain;
-using Application;
 
 namespace Application.Core
 {
@@ -17,10 +16,22 @@ namespace Application.Core
       CreateMap<Activity, ActivityDto>()
           .ForMember(d => d.HostUsername, o => o.MapFrom(u => u.Attendees
             .FirstOrDefault(x => x.IsHost).AppUser.UserName));
-      CreateMap<ActivityAttendee, Profiles.Profile>()
-        .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
-        .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
-        .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+
+      CreateMap<ActivityAttendee, AttendeeDto>()
+        .ForMember(d => d.DisplayName, o => o
+          .MapFrom(s => s.AppUser.DisplayName))
+        .ForMember(d => d.Username, o => o
+          .MapFrom(s => s.AppUser.UserName))
+        .ForMember(d => d.Bio, o => o
+          .MapFrom(s => s.AppUser.Bio))
+        .ForMember(a => a.Image, o => o
+          .MapFrom(s => s.AppUser.Photos
+            .FirstOrDefault(x => x.IsMain).Url));
+
+      CreateMap<AppUser, Profiles.Profile>()
+        .ForMember(a => a.Image, o => o
+          .MapFrom(s => s.Photos
+            .FirstOrDefault(x => x.IsMain).Url));
     }
   }
 }
