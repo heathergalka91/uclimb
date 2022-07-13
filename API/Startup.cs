0aefd87ad3daf043f30550afccd0cff6c9059428
Heather Gakla <Heather.Galka.91@gmail.com>
2022-07-13 10:47:02 -0600

    Security headers added

--- API/Startup.cs
+++ API/Startup.cs
@@ -50,10 +50,32 @@ namespace API
     {
       app.UseMiddleware<ExceptionMiddleware>();
 
+      app.UseXContentTypeOptions();
+      app.UseReferrerPolicy(opt => opt.NoReferrer());
+      app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+      app.UseXfo(opt => opt.Deny());
+      app.UseCspReportOnly(opt => opt
+        .BlockAllMixedContent()
+        .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://cdn.jsdelivr.net"))
+        .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "https://cdn.jsdelivr.net", "data:"))
+        .FormActions(s => s.Self())
+        .FrameAncestors(s => s.Self())
+        .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+        .ScriptSources(s => s.Self().CustomSources("sha256-c3WD0pwpDoMzew+bMDkFm4XwgR2Y8Rd23PL2PuWBfTw="))
+      );
+
       if (env.IsDevelopment())
       {
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+      }
+      else
+      {
+        app.Use(async (context, next) =>
+        {
+          context.Response.Headers.Add("Strict-Transport-Security", "max-age=31566000");
+          await next.Invoke();
+        });
       }
 
       //app.UseHttpsRedirection();
